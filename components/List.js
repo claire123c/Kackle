cfefa7f6f88af3c54131ebd7b3d260e0997d96a9
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-07 21:20:06 -0700

    Added item list

--- components/List.js
+++ components/List.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { Text, View, Button } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
+import { ListItem } from 'react-native-elements'
+import styles from '../assets/styles/index.js';
 
 export default function Header() {
   return (
     <View>
+      <ListItem>
+        <ListItem.Title>
+          Restaurant 1
+        </ListItem.Title>
+      </ListItem>
       <Text>Restaurant 1</Text>
       <Text>Restaurant 2</Text>
       <Text>Restaurant 3</Text>
