95a6e9997541f9c02c546174172d15e9bf9e1889
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-07 21:25:34 -0700

    Added header button

--- navigators/MainStackNavigator.js
+++ navigators/MainStackNavigator.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { Button } from 'react-native';
 
 import Home from '../screens/Home.js';
 import Recs from '../screens/Recs.js';
 
 const Stack = createStackNavigator()
 
+const homeOptions = {
+  headerRight: () => (
+    <Button title='YOYO'/>
+  )
+};
+
 export default function MainStackNavigator() {
   return (
     <NavigationContainer >
       <Stack.Navigator>
-        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Home' component={Home} options={homeOptions}/>
         <Stack.Screen name='Recs' component={Recs} />
       </Stack.Navigator>
     </NavigationContainer>
