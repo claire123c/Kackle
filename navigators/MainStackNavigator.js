68f30adb0ee2751db648f9411de26cc02cc4b736
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-12 01:45:39 -0700

    Added new settings animation and superlike icon

--- navigators/MainStackNavigator.js
+++ navigators/MainStackNavigator.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { TransitionPresets } from '@react-navigation/stack';
 import styles from '../assets/styles/index.js';
 import Home from '../screens/Home.js';
 import Recs from '../screens/Recs.js';
@@ -11,14 +12,20 @@ const SCREEN1 = 'Kackle';
 const SCREEN2 = 'Future Eats';
 const SCREEN3 = 'Settings';
 
+const screen3Options = {
+  animationEnabled: false
+};
+
 export default function MainStackNavigator() {
 
   return (
     <NavigationContainer >
       <Stack.Navigator>
         <Stack.Screen name={SCREEN1} component={Home}/>
-        <Stack.Screen name={SCREEN2} component={Recs} />
-        <Stack.Screen name={SCREEN3} component={Settings} />
+        <Stack.Screen name={SCREEN2} component={Recs} keyboardHandlingEnabled={true}/>
+        <Stack.Screen name={SCREEN3} component={Settings} options={{
+         ...TransitionPresets.ModalSlideFromBottomIOS,
+      }}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
