fc82b994bd4f9fa05748430455ece31dcb228681
Claire Chen <57816663+claire123c@users.noreply.github.com>
2021-07-14 23:59:03 -0700

    creating api call for bix

--- screens/FoodDetails.js
+++ screens/FoodDetails.js
@@ -4,11 +4,16 @@ import { Text, View } from 'react-native';
 import styles from '../assets/styles/index.js';
 
 
-export default function FoodDetails() {
+export default function FoodDetails({ route }) {
+  const id = route.params;
 
   const getBizDeets = () => {
 
   }
+
+  useEffect(() => {
+
+  }, [route]);
 
   return (
   <View>
